cdf52004bbc33fb76c313c0c2a41d5836cccfe39
HasanAhmed111 <144584937+HasanAhmed111@users.noreply.github.com>
2023-09-26 14:08:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+//    CHAPTER 21
+//    Q1
+var arr = ["karachi","lahore"];
+var userinput = prompt("Where do you live?");
+var userinput = userinput.toLowerCase();
+console.log(userinput);
+//    Q2
+var x = "pakistan";
+var x = x.toLowerCase();
+console.log(x);
+    //  Q3
+var y = "hasan";
+var y = y.toUpperCase();
+console.log(y);
+// Q4
+var a = "pagani";
+var a = a.toLowerCase();
+var b = a;
+console.log(b);
+//   Q5
+// var c = ["mango","kiwi","papaya"];
+// var c = c.toLowerCase();
+// var d = c;
+// console.log(c);
+//   Q6
+var lastName = "shaikh";
+lastName = lastName.toUpperCase();
+alert(lastName);
+// Q7
+var cityName = "Karachi";
+var firstChar = cityName.slice(0,1).toUpperCase();
+var remainChar = cityName.slice(1).toLowerCase();
+var cityName = firstChar + "" + remainChar;
+console.log(cityName);
+  
+
+// CHAPTER 22
+// Q1
+var sameWords = "captain";
+console.log(sameWords.slice(0,2));
+// Q2
+var str = "hasan ahmed";
+console.log(str.length);
+// Q3
+var animal = "elephant";
+var seg = animal.slice(2,6);
+console.log(seg);
+// Q4
+var dfs = "hgstrdag";
+var sedf = dfs.length;
+console.log(sedf);
+// Q5
+
